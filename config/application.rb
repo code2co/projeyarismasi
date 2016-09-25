7721a42a2fa48dbf8b3290912deef1610dde8ee7
Utku Kaynar <utku@code2.co>
2016-09-25 17:09:22 +0300

    Added a home page, general styling for typography etc

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Projeyarismasi
     end
     config.action_controller.action_on_unpermitted_parameters = :raise
     config.active_job.queue_adapter = :delayed_job
+    # config.i18n.default_locale = :tr
   end
 end
