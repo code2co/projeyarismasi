d0a3e5c4e172d67af2710d16dd344e618f23c105
Utku Kaynar <utku@code2.co>
2016-10-19 16:48:09 +0300

    Correct emaıl on devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'utku@code2.co'
+  config.mailer_sender = 'bepy@emo.org.tr'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
